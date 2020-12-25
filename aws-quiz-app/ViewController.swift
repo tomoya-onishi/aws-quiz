1d2af878e641b35355e05164dbcb0f39e5c4839c
tomoya-onishi <tomoya-onishi@x-power.co.jp>
2020-12-25 16:18:22 +0900

    first commit

---
+++ aws-quiz-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aws-quiz-app
+//
+//  Created by 大西智也 on 2020/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
