4d0f7c24f972a25116fdef651d2ad786f6b1a9a7
大西智也 <tomoya@oonishitomoyanoMacBook-Pro.local>
2021-01-05 01:08:19 +0900

    Second commit

---
+++ aws-quiz-app/QuizViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuizViewController.swift
+//  aws-quiz-app
+//
+//  Created by 大西智也 on 2020/12/29.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ aws-quiz-app/ScoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScoreViewController.swift
+//  aws-quiz-app
+//
+//  Created by 大西智也 on 2020/12/29.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
